453465f013ee9545fe11247dd60b61d9cd92b458
Martin Cook <martin@thornhill.co.za>
2013-06-27 22:19:21 +0200

    Seed data: added surgeries to people.
    Also added script to reset database

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -26,9 +26,10 @@ class PeopleTableSeeder extends Seeder {
         $firstnames = array('Fred', 'Joe', 'Bob', 'Jordan', 'Precious', 'Lindi', 'Samuel');
         $surnames = array('Brown', 'Smith', 'Mavundla', 'Bloggs', 'Dlamini', 'Jackson', 'Fredericks');
         $hospitalnumbers = array('12345432', '4546532', '3426562', '3246', '348264', '7469301', '38523');
-        $grades = array('1', '4', NULL, '3', NULL, '1', '1');
+        $grades = array('1', '4', NULL, '3', '2', '1', '1');
         $date_booked = array('21 June 2012', '23 July 2012', '3 May 2012', '29 December 2012', '23 April 2013',
             '16 June 2015', '2 Feb 2014');
+        $surgery_types = array('1', '1', '3', '2','6','1','2');
         foreach ($firstnames as $key=>$name) {
             Person::create(array('first_name' => $firstnames[$key], 'surname' => $surnames[$key],
                 'hospital_number' => $hospitalnumbers[$key], 'grade' => $grades[$key],
@@ -48,6 +49,12 @@ class PeopleTableSeeder extends Seeder {
         Surgerytype::create(array('name'=>'Trab' , "group"=>null));
         Surgerytype::create(array('name'=>'Conj Mass Excision', "group"=>2));
         Surgerytype::create(array('name'=>'Other Extra-ocular Procedures', "group"=>2));
+
+        $i = 0;
+        foreach ($surgery_types as $s) {
+            $i++;
+            Surgery::create(array('person_id' => $i, 'surgerytype_id' => $s));
+        }
 
 
     }
